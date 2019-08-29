041449ff2ec5772ae1687539e906d08c86d10281
Patton97 <32955550+Patton97@users.noreply.github.com>
2019-08-29 02:38:28 +0100

    Add files via upload

--- Dengar/main.js
+++ Dengar/main.js
@@ -7,11 +7,12 @@
 /************************/
 //Autozoom | https://benjymous.gitlab.io/post/2019-08-25-js13k-tips/
 onresize=e=>{
-  // Zoom the page to maximise content (Assume 640x480 canvas + 640x240 gamepad)
-  document.body.style.zoom=Math.min(window.innerWidth/640, window.innerHeight/720);
+  // Zoom the page to maximise content
+  // Height = canvas=480 + gamepad=240 + gap= . Gap is bug fix.
+  document.body.style.zoom=Math.min(window.innerWidth/640, window.innerHeight/900);
   //document.body.style.zoom=1;
   // Add padding at the top of the page, to centre the content vertically
-  document.body.style.paddingTop=((window.innerHeight/document.body.style.zoom)-720)/2;
+  document.body.style.paddingTop=((window.innerHeight/document.body.style.zoom)-900)/2;
 };
 onresize(); //Force resize
 
@@ -30,12 +31,18 @@ var spritesheet = {
 }
 
 var character = {
+  x: 600,  y:0,
+  sx: 0, sy: 0,
+  xVel:   0,  yVel:0,
   jumping: true,
-  xPos: 600,  yPos:0,
-  xVel:0,  yVel:0,
-  //Spritesheet
-  img: document.getElementById('charSpritesheet'),
-  sx: 0, sy: 0
+  draw:function()
+  {
+    //Choose character sprite
+    //  TODO: Move code here
+    context.drawImage(spritesheet.img,
+                      this.sx,   this.sy,   TILESIZE, TILESIZE,  //source image data
+                      this.x, this.y, TILESIZE, TILESIZE); //dest.  image data
+  }
 };
 
 //Stores img data on non-collidable tiles
@@ -53,10 +60,10 @@ class bgTile
 
 class Scrollable
 {
-  constructor(height = 1, yPos = 0, speed = 1, sy = 0)
+  constructor(height = 1, y = 0, speed = 1, sy = 0)
   {
     this.height = height;
-    this.yPos = yPos;
+    this.y = y;
     this.speed = speed;
     this.sy = sy;
     this.strips = [];
@@ -100,7 +107,7 @@ class Scrollable
         let sx = this.strips[i][j].sx;
         let sy = this.strips[i][j].sy;
         let dx = (context.canvas.width - (i+1)*TILESIZE) + Math.floor(this.offset);
-        let dy = j*TILESIZE + this.yPos;
+        let dy = j*TILESIZE + this.y;
         context.drawImage(spritesheet.img,
                           sx, sy, TILESIZE, TILESIZE,  //source image data
                           dx, dy, TILESIZE, TILESIZE); //dest.  image data
@@ -111,6 +118,11 @@ class Scrollable
     {
       this.makeNewStrip();
       this.offset = 0;
+      //20% Chance to spawn cactus - Possibly just temp solution
+      if(Math.floor(Math.random() * 9) % 5 === 0)
+      {
+        let cactus = new Cactus(-TILESIZE);
+      }
     }
   }
 }
@@ -137,7 +149,7 @@ var controller = {
         controller.right = key_state;
         break;
       default:
-        console.log(event.keyCode + "pressed");
+        console.log("Key #" + event.keyCode + " pressed");
         break;
     }
   }
@@ -199,16 +211,124 @@ touchR.addEventListener(u,e=>{
 
 //End of Gamepad
 
-//Draw character
-drawCharacter = function()
+
+var ObjectManager =
 {
-  //Animate "walk"
-  //Looks like shit
-  //if(!character.jumping && iFrame > 50) { character.sy = 32;  }
-  context.drawImage(spritesheet.img,
-                    character.sx,   character.sy,   TILESIZE, TILESIZE,  //source image data
-                    character.xPos, character.yPos, TILESIZE, TILESIZE); //dest.  image data
+  gameObjects: [],
+  update:function()
+  {
+    this.purgeInactive();
+    this.checkAllCollisions();
+    this.drawAll();
+  },
+  purgeInactive:function()
+  {
+    for(let i = 0; i < this.gameObjects.length; i++)
+    {
+      if(!this.gameObjects[i].active)
+      {
+        //You'd think this is a stupid idea and would break everything, but as
+        // long as you don't splice 2 objects at the same time, it's safe :D
+        delete this.gameObjects[i];
+        this.gameObjects.splice(i,1);
+      }
+    }
+  },
+  checkAllCollisions:function()
+  {
+    for(let i = 0; i < this.gameObjects.length; i++)
+    {
+      this.gameObjects[i].checkCollisions();
+    }
+  },
+  drawAll:function()
+  {
+    for(let i = 0; i < this.gameObjects.length; i++)
+    {
+      this.gameObjects[i].draw();
+    }
+  }
 }
+
+class GameObject
+{
+  constructor(x=0,y=0,width=TILESIZE,height=TILESIZE,sx=0,sy=0,collidable=false)
+  {
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.sx=sx;
+    this.sy=sy;
+    this.collidable=collidable;
+    this.active=true;
+    ObjectManager.gameObjects.push(this);
+  }
+  draw()
+  {
+    //Objects should move with floor
+    this.x += floor.speed * (16/60);
+    //Expanded for readability
+    context.drawImage(spritesheet.img,         //Source spritesheet
+                      this.sx,    this.sy,     //Source coords
+                      this.width, this.height, //Source dimensions
+                      this.x,     this.y,      //Destination coords
+                      this.width, this.height);//Destination dimensions
+    //If object is now offscreen, deactivate
+    this.active = this.x < context.canvas.width;
+  }
+  checkCollisions()
+  {
+    //If object isn't collidable, skip check
+    if(this.collidable)
+    {
+      //AABB collision test
+      if(character.x < this.x + this.width
+      && character.x + TILESIZE > this.x
+      && character.y < this.y + this.height
+      && character.y + TILESIZE > this.y)
+      {
+        //TODO: Game over sequence
+        //For now, player just bounces off
+        character.xVel = 10;
+        return true;
+      }
+    }
+    //If code reaches this point, no collision detected
+    return false;
+  }
+}
+
+class Cactus extends GameObject
+{
+  constructor(x)
+  {
+    //Randomly select which row to place cactus in
+    let y = floor.y+(TILESIZE*(Math.floor(Math.random()*5) - 1));
+    //Randomly select which cactus sprite to use
+    let sx = 96 + (TILESIZE * Math.floor(Math.random() * 2));
+    let sy = 64;
+    super(x,y,TILESIZE,TILESIZE,sx,sy,true);
+  }
+}
+
+var prevFrameTime=0;
+var fps=0;
+function drawFPS(iFrame)
+{
+  //Update value every 10 frames for better readability
+  if(iFrame % 10 === 0)
+  {
+    fps = Math.floor(1000/(performance.now()-prevFrameTime));
+  }
+  prevFrameTime = performance.now();
+  //Draw text every frame to avoid flickering
+  context.font = "normal bold 1em courier new";
+  context.fillStyle = "yellow";
+  context.textAlign = "right";
+  context.fillText(fps, context.canvas.width,10);
+}
+
 
 var gameLoop = function()
 {
@@ -222,19 +342,18 @@ var gameLoop = function()
   }
   if(controller.left)  { character.xVel -= .5; character.sx=0; }
   if(controller.right) { character.xVel += .5; character.sx=64;}
-
   //Physics
   character.yVel += 1; //Gravity
-  character.xPos += character.xVel;
-  character.yPos += character.yVel;
+  character.x += character.xVel;
+  character.y += character.yVel;
   character.xVel *= 0.9; //Friction (x)
 
-  //Collisions
+  //Check Boundaries
   //Floor
-  if(character.yPos + TILESIZE > floor.yPos)
+  if(character.y + TILESIZE > floor.y)
   {
     character.jumping = false;
-    character.yPos = floor.yPos - TILESIZE;
+    character.y = floor.y - TILESIZE;
     character.yVel = 0;
     character.sy = 0;
   }
@@ -242,10 +361,9 @@ var gameLoop = function()
   {
     character.sy = 64;
   }
-
   //Sides of the screen
-  if(character.xPos < 0) {character.xPos = 1; }
-  if(character.xPos+TILESIZE > context.canvas.width) {character.xPos = context.canvas.width-TILESIZE}
+  if(character.x < 0) {character.x = 1; }
+  if(character.x+TILESIZE > context.canvas.width) {character.x = context.canvas.width-TILESIZE}
 
   //Drawing
   //Draw background
@@ -253,9 +371,15 @@ var gameLoop = function()
   context.fillRect(0,0,context.canvas.width,context.canvas.height);
 
   //Construct next frame
+  //Needs organising
   floor.draw();
   sky.draw();
-  drawCharacter();
+  character.draw();
+  ObjectManager.update();
+  console.log(ObjectManager.gameObjects.length);
+
+  //DEBUG ONLY
+  drawFPS(iFrame);
 
   //Update iframe (yes ugly ew)
   if(iFrame < 60){iFrame++;}else{iFrame = 0;}
