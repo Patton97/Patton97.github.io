e02860964c4b9a1b8ffd3974c14012aebd283203
Patton97 <atpatton97@gmail.com>
2020-03-31 19:28:07 +0100

    Portfolio now rendered from JSON data

---
+++ portfolio.js
@@ -0,0 +1,92 @@
+
+function GenerateHeaderHTML(id, title, tools)
+{
+  let html = ``
+
+  html += `<span class="projectCardHeader" id ="${id}">`
+  html += `  <h2>${title}</h2>`
+  html += `  <ul class="tools">`
+
+  tools.forEach(tool => {
+    html += `    <li><a href="" class="${tool}"></a></li>`
+  });
+
+  html += `  </ul>`
+  html += `</span>`
+
+  return html
+}
+
+function GenerateDescriptionHTML(description)
+{
+  let html = ``
+  html += `<p class="description">${description}</p>`
+  return html
+}
+
+function GenerateVideoHTML(video)
+{
+  let html = ``
+  if(video == null) {return html}
+
+  
+  html += `<video width="75%" autoplay controls loop muted>`
+  html += `  <source src="${video}" type="video/mp4">`
+  html += `  Your browser does not support the video tag.`
+  html += `</video>`
+  return html
+}
+
+function GenerateLinksHTML(links)
+{
+  let html = ``
+  html += `<ul class="links">`
+  links.forEach(link =>{
+    html += `<li><a href="${link.url}" target="_blank" class="${link.class}">${link.text}</a></li>`
+  });
+  html += `</ul>`
+  return html
+}
+
+function GenerateProjectHTML(project)
+{
+  let section = document.createElement('section')
+  section.className = "projectCard"
+
+  let html = ``
+  html += GenerateHeaderHTML(project.id, project.title, project.tools)
+  html += GenerateDescriptionHTML(project.description)
+  html += GenerateVideoHTML(project.video)
+  html += GenerateLinksHTML(project.links)
+
+  section.innerHTML = html
+
+  return section
+}
+
+function GeneratePortfolioHTML(projects)
+{
+  let main = document.getElementsByTagName("main")[0]
+  projects.forEach(project => {
+    let html = GenerateProjectHTML(project)
+    main.insertBefore(html, main.lastChild)
+  });
+}
+
+var json = (function () 
+{
+  var json = null;
+  $.ajax({
+      'async': false,
+      'global': false,
+      'url': './portfolio.json',
+      'dataType': "json",
+      'success': function (data) {
+          json = data;
+      }
+  });
+  return json;
+})(); 
+
+GeneratePortfolioHTML(json)
+    
