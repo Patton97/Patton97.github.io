43f3b3cef2c845404253e2e98c82c084db63d2ed
Patton97 <atpatton97@gmail.com>
2025-03-28 20:51:25 +0000

    Switching twitter to linkedin

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ function GenerateFooterHTML()
   let html = ``
   html += `<ul class="links">`
   html += `  <li><a href="mailto:atpatton97@gmail.com" class="email">atpatton97@gmail.com</a></li>`
-  html += `  <li><a href="https://twitter.com/andrewpattondev" class="twitter">@andrewpattondev</a></li>`
+  html += `  <li><a href="https://linkedin.com/in/andrewpattondev" class="linkedin">/in/andrewpattondev</a></li>`
   html += `</ul>`
   return html
 }
