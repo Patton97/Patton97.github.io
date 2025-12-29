db5ff24271586bad4e63b3ee492539d96aa6fe80
Andrew Patton <32955550+Patton97@users.noreply.github.com>
2025-12-29 12:30:46 +0000

    Fixed formatting in localserver.py

--- localserver.py
+++ localserver.py
@@ -10,14 +10,14 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 Handler.extensions_map={
-        '.manifest': 'text/cache-manifest',
-	'.html': 'text/html',
-        '.png': 'image/png',
-	'.jpg': 'image/jpg',
-	'.svg':	'image/svg+xml',
-	'.css':	'text/css',
-	'.js':	'application/x-javascript',
-	'': 'application/octet-stream', # Default
+    '.manifest': 'text/cache-manifest',
+    '.html'    : 'text/html',
+    '.png'     : 'image/png',
+    '.jpg'     : 'image/jpg',
+    '.svg'     : 'image/svg+xml',
+    '.css'     : 'text/css',
+    '.js'      : 'application/x-javascript',
+    ''         : 'application/octet-stream', # Default
     }
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
