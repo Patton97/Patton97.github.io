d587b8f3a57f2ae0f248b94ef088d2c01d6ae407
Andrew Patton <32955550+Patton97@users.noreply.github.com>
2026-01-19 20:43:33 +0000

    Improved layout of showcased project cards

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -1,105 +1,129 @@
-function GenerateHeaderHTML(id, title, tools)
+function GenerateProjectCardHeaderElement(id, title, tools)
 {
-  let html = ``
+  let headerElement = document.createElement("div");
+  headerElement.className = "projectCardHeader";
+  headerElement.id = id;
 
-  html += `<span class="projectCardHeader" id ="${id}">`
-  html += `  <h2>${title}</h2>`
-  html += `  <ul class="tools">`
+  let headerTextElement = document.createElement("h2");
+  headerTextElement.textContent = title;
+  headerElement.appendChild(headerTextElement);
 
-  tools.forEach(tool => {
-    html += `    <li><a class="${tool}"><span class="tooltip"></span></a></li>`
-  });
+  let toolsListElement = document.createElement("ul");
+  toolsListElement.className = "tools";
+  tools.forEach(tool => toolsListElement.innerHTML += `<li><a class="${tool}"><span class="tooltip"></span></a></li>`);
+  headerElement.appendChild(toolsListElement);
 
-  html += `  </ul>`
-  html += `</span>`
-
-  return html
+  return headerElement;
 }
 
-function GenerateDescriptionHTML(description)
+function GenerateDescriptionElement(description)
 {
-  let html = ``
-  html += `<p class="description">${description}</p>`
-  return html
+  let descriptionElement = document.createElement("p");
+  descriptionElement.className = "description";
+  descriptionElement.textContent = description;
+  return descriptionElement;
 }
 
-function GenerateImageHTML(image)
+function GenerateMediaElement(project)
 {
-  let html = ``
-  if(image == null) {return html}
+  let mediaElement = document.createElement("div");
+  mediaElement.className = "mediaWrapper";
 
-  html += `<div class="videoWrapper">`
-  html += `  <img src="${image.url}" alt="NDA"/>`
-  html += `</div>`
-  return html
-}
-
-function GenerateVideoHTML(video)
-{
-  let html = ``
-  if(video == null) {return html}
-
-  if(video.scale == null) { video.scale = 100 }
-
-  html += `<div class="videoWrapper">`
-  html += `  <video autoplay controls loop muted>`
-  html += `    <source src="${video.url}" type="video/mp4">`
-  html += `    Your browser does not support the video tag.`
-  html += `  </video>`
-  html += `</div>`
-  return html
-}
-
-function GenerateLinksHTML(links)
-{
-  if (!Array.isArray(links) || links.length == 0)
+  if (project.video)
   {
-    return ``
+    mediaElement.innerHTML = `
+<video autoplay controls loop muted>
+  <source src="${project.video.url}" type="video/mp4">
+  Your browser does not support the video tag.
+</video>
+`;
+  }
+  else if (project.image)
+  {
+    mediaElement.innerHTML = `<img src="${project.image.url}" alt="NDA"/>`;
+  }
+  else
+  {
+    return null;
   }
 
-  let html = ``
-  html += `<ul class="links">`
-  links.forEach(link =>{
-    html += `<li><a href="${link.url}" target="_blank" rel="noopener noreferrer" class="${link.class}">${link.text}</a></li>`
-  });
-  html += `</ul>`
-  return html
+  return mediaElement;
 }
 
-function GenerateProjectHTML(project)
+function GenerateLinksElement(links)
 {
-  let html = ``
-  html += GenerateHeaderHTML(project.id, project.title, project.tools)
-  html += GenerateDescriptionHTML(project.description)
-  html += GenerateVideoHTML(project.video)
-  html += GenerateImageHTML(project.image)
-  html += GenerateLinksHTML(project.links)
+  if (!Array.isArray(links) || links.length === 0)
+  {
+    return null;
+  }
 
-  return html
+  let linksElement = document.createElement("ul");
+  linksElement.className = "links";
+  links.forEach(link => linksElement.innerHTML += `<li><a href="${link.url}" target="_blank" rel="noopener noreferrer" class="${link.class}">${link.text}</a></li>`);
+  
+  return linksElement;
+}
+
+function CreateProjectCardElement(project)
+{
+  let projectCard = document.createElement("div");
+  projectCard.className = "projectCard";
+
+  projectCard.appendChild(GenerateProjectCardHeaderElement(project.id, project.title, project.tools));
+  projectCard.appendChild(GenerateDescriptionElement(project.description));
+
+  let mediaElement = GenerateMediaElement(project);
+  if (mediaElement)
+  {
+    projectCard.appendChild(mediaElement);
+  }
+
+  let linksElement = GenerateLinksElement(project.links);
+  if (linksElement)
+  {
+    projectCard.appendChild(linksElement);
+  }
+
+  return projectCard;
 }
 
 function GenerateShowcaseHTML(showcaseTitle, projects)
 {
-  let main = document.getElementsByTagName("main")[0]
+  let main = document.getElementsByTagName("main")[0];
 
-  let showcaseTitleElement = document.createElement("h1")
-  showcaseTitleElement.textContent = showcaseTitle
-  main.insertBefore(showcaseTitleElement, main.lastChild)
+  // Showcase section
+  let showcaseSection = document.createElement("section");
+  showcaseSection.className = "showcase";
+  main.insertBefore(showcaseSection, main.lastChild);
 
-  let showcaseSection = document.createElement("section")
-  showcaseSection.className = "showcase"
-  main.insertBefore(showcaseSection, main.lastChild)
+  // Showcase title
+  let showcaseTitleElement = document.createElement("h1");
+  showcaseTitleElement.textContent = showcaseTitle;
+  showcaseSection.appendChild(showcaseTitleElement);
 
-  let showcaseGrid = document.createElement("div")
-  showcaseGrid.className = "showcaseGrid"
-  showcaseSection.appendChild(showcaseGrid)
+  // Project group container
+  let projectGroup = document.createElement("div");
+  projectGroup.className = "projectGroup";
+  showcaseSection.appendChild(projectGroup);
 
+  // Add project cards
   projects.forEach(project => {
-    let showcaseGridCell = document.createElement("div")
-    showcaseGridCell.className = "showcaseGridCell"
-    showcaseGridCell.innerHTML = GenerateProjectHTML(project)
-    showcaseGrid.appendChild(showcaseGridCell)
+    let projectCard = CreateProjectCardElement(project);
+    projectGroup.appendChild(projectCard);
   });
+}
+
+function LoadProjectJson(jsonUrl)
+{
+   var json = null
+    $.ajax({
+        'async': false,
+        'global': false,
+        'url': jsonUrl,
+        'dataType': "json",
+        'success': function (data) { json = data; }
+    })
+    return json
 }
 
 function GetProjectsForShowcase(projects, showcaseId)
@@ -110,43 +134,17 @@ function GetProjectsForShowcase(projects, showcaseId)
     {
       return !project.hasOwnProperty("showcase")
     }
-
     return project.showcase === showcaseId
   })
 }
 
-function LoadProjectJson(jsonUrl)
-{
-  var json = null
-  $.ajax({
-      'async': false,
-      'global': false,
-      'url': jsonUrl,
-      'dataType': "json",
-      'success': function (data)
-      {
-        json = data;
-      }
-  })
-  return json
-}
+let portfolioJson = LoadProjectJson('/Portfolio/portfolio.json');
+let personalShowcaseJson = GetProjectsForShowcase(portfolioJson, "personal");
+console.log("Found " + portfolioJson.length + " personal projects!");
+console.log(portfolioJson);
+GenerateShowcaseHTML("⭐Personal Showcase⭐", personalShowcaseJson);
 
-let portfolioJson = LoadProjectJson('/Portfolio/portfolio.json')
-
-let personalShowcaseJson = GetProjectsForShowcase(portfolioJson, "personal")
-console.log("Found " + portfolioJson.length + " personal projects!")
-console.log(portfolioJson)
-GenerateShowcaseHTML("⭐Personal Showcase⭐", personalShowcaseJson)
-
-let professionalShowcaseJson = GetProjectsForShowcase(portfolioJson, "professional")
-console.log("Found " + professionalShowcaseJson.length + " professional projects!")
-console.log(professionalShowcaseJson)
-
-GenerateShowcaseHTML("⭐Professional Showcase⭐", professionalShowcaseJson)
-
-// TODO: Find a nice way of styling a showcase with more than 1 row of projects
-// TODO: Make this a button, which only loads them if the user actively requests it (save on video download)
-// let archiveJson = GetProjectsForShowcase(portfolioJson, null)
-// console.log("Found " + archiveJson.length + " archive projects!")
-// console.log(archiveJson)
-// GenerateShowcaseHTML("Archive", archiveJson)
+let professionalShowcaseJson = GetProjectsForShowcase(portfolioJson, "professional");
+console.log("Found " + professionalShowcaseJson.length + " professional projects!");
+console.log(professionalShowcaseJson);
+GenerateShowcaseHTML("⭐Professional Showcase⭐", professionalShowcaseJson);
